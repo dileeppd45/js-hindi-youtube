ba0089aa2ea55ae75124e77303dd59addb80a782
DILEEP <dileep.fullstack.dev@gmail.com>
2026-06-13 05:28:42 +0530

    conversion operations investigated

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+
+
+
+ 
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+// let isLoggedIn = 1
+// let isLoggedIn = "p"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false; "p" => true;
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof(stringNumber))
+console.log(stringNumber)
