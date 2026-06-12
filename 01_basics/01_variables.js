e932100d009009fbabd4f26574d7bb8095529223
DILEEP <dileep.fullstack.dev@gmail.com>
2026-06-12 16:17:35 +0530

    covered 4 and 5 videos

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "dileeppd45@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 //not allowed
+
+accountEmail = "pddileep@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log(3
+     + 
+     3) // code readability should be heigh
+
+console.log("Dileep")
+
+let name ="dileep"
+let age = 27
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)
+
